227a64a4124434e051e1638a687a62515e951a8e
aleksas <aasmc@yandex.ru>
2023-01-31 11:56:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taco Cloud
+
+Educational App based on the book Spring in Action 6-th edition. 
---
+++ src/test/java/ru/aasmc/tacoclound/TacoCloundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aasmc.tacoclound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
