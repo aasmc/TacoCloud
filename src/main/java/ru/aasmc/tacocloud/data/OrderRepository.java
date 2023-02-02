0845bd36f87c42e8aa1580bcae2c9da17e2d4fd7
aleksas <aasmc@yandex.ru>
2023-02-02 15:34:14 +0300

    Add custom methods to OrderRepository

--- src/main/java/ru/aasmc/tacocloud/data/OrderRepository.java
+++ src/main/java/ru/aasmc/tacocloud/data/OrderRepository.java
@@ -3,6 +3,13 @@ package ru.aasmc.tacocloud.data;
 import org.springframework.data.repository.CrudRepository;
 import ru.aasmc.tacocloud.TacoOrder;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+import java.util.Date;
+import java.util.List;
 
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryNameAndDeliveryCityAllIgnoringCase(String deliveryName, String deliveryCity);
 }
